fa3763bc6857648d61e6d3cf9c7a8e10041a36b8
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2018-06-11 15:46:25 +0200

    add xvfb.service

--- bin/run_f2x.sh
+++ bin/run_f2x.sh
@@ -2,15 +2,15 @@
 set -e
 cd "$(cd "$(dirname "$0")"; pwd)/.."
 export ORACLE_BASE=/oracle/mw11gR1
-export ORACLE_HOME=$ORACLE_BASE/fr11gR2
+export ORACLE_HOME="$ORACLE_BASE/fr11gR2"
 # /oracle/mw11gR1/fr11gR2/lib/libfrmjapi.so.0
-export LD_LIBRARY_PATH=$ORACLE_HOME/bin:$ORACLE_HOME/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$ORACLE_HOME/bin:$ORACLE_HOME/lib:$LD_LIBRARY_PATH"
 export TERM=xterm
 
-CLASSPATH=$CLASSPATH:classes:$ORACLE_HOME/jlib/frmjdapi.jar:$ORACLE_HOME/jlib/frmxmltools.jar:$ORACLE_HOME/lib/xmlparserv2.jar
+CLASSPATH="classes:$ORACLE_HOME/jlib/frmjdapi.jar:$ORACLE_HOME/jlib/frmxmltools.jar:$ORACLE_HOME/lib/xmlparserv2.jar"
 if ! [ "${NOCOMPILE:-0}" -eq 1 ]; then
 	mkdir -p classes
-	javac -cp $CLASSPATH -d classes src/unosoft/forms/Serve.java
+	javac -cp "$CLASSPATH" -d classes src/unosoft/forms/Serve.java
 fi
 export -p
 
@@ -22,7 +22,7 @@ if [ -z "$DISPLAY" ]; then
 	echo
 fi
 set -x
-exec java -cp "$CLASSPATH" -Djava.library.path=$ORACLE_HOME/lib \
+exec java -cp "$CLASSPATH" "-Djava.library.path=$ORACLE_HOME/lib" \
 	"-Dforms.lib.path=$FORMS_LIB" \
 	"-Dforms.db.conn=$DB_CONN" \
 	unosoft.forms.Serve "$@"
