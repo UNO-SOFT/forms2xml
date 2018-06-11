4bb8631c22ac21772afef48b75c1ce7bc2fed864
Tamas Gulacsi <tgulacsi78@gmail.com>
2018-06-11 15:02:36 +0200

    print out that DISPLAY is required

--- run_f2x.sh
+++ run_f2x.sh
@@ -5,16 +5,22 @@ export ORACLE_BASE=/oracle/mw11gR1
 export ORACLE_HOME=$ORACLE_BASE/fr11gR2
 # /oracle/mw11gR1/fr11gR2/lib/libfrmjapi.so.0
 export LD_LIBRARY_PATH=$ORACLE_HOME/bin:$ORACLE_HOME/lib:$LD_LIBRARY_PATH
-export DISPLAY=${DISPLAY:-"localhost:10.0"}
 export TERM=xterm
 
 CLASSPATH=$CLASSPATH:classes:$ORACLE_HOME/jlib/frmjdapi.jar:$ORACLE_HOME/jlib/frmxmltools.jar:$ORACLE_HOME/lib/xmlparserv2.jar
-set -x
 if ! [ "${NOCOMPILE:-0}" -eq 1 ]; then
 	mkdir -p classes
 	javac -cp $CLASSPATH -d classes src/unosoft/forms/Serve.java
 fi
 export -p
+
+if [ -z "$DISPLAY" ]; then
+	echo
+	echo '*********************************'
+	echo "* DISPLAY is empty, won't work! *" >&2
+	echo '*********************************'
+	echo
+fi
 set -x
 exec java -cp "$CLASSPATH" -Djava.library.path=$ORACLE_HOME/lib \
 	"-Dforms.lib.path=$FORMS_LIB" \
