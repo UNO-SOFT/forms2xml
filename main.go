98320b61cdf0088b89e4cc4af8c4b9827ba72a25
Tamás Gulácsi <tgulacsi78@gmail.com>
2018-06-11 09:27:36 +0200

    try windows

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
+// Copyright 2018 Tamás Gulácsi
+//
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
 package main
 
 import (
@@ -76,7 +91,7 @@ func Main() error {
 func watchConvert(dstDir, srcDir string, doTransform bool, suffix string, concurrency int) error {
 	tokens := make(chan struct{}, concurrency)
 	eventCh := make(chan notify.EventInfo, 16)
-	if err := notify.Watch(srcDir, eventCh, notify.InCloseWrite, notify.InMovedTo); err != nil {
+	if err := notify.Watch(srcDir, eventCh, eventsToWatch...); err != nil {
 		return err
 	}
 	for evt := range eventCh {
